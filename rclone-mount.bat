832e9f9d3607fee9c73d5f399b2176e7e66eb53d
FoxP <FoxP@users.noreply.github.com>
2018-08-05 10:14:45 +0200

    Add more rclone flags

--- rclone-mount.bat
+++ rclone-mount.bat
@@ -7,6 +7,7 @@ REM The mountpoint must be an unused drive letter
 REM Configuration variables
 SET RCLONE_EXE_PATH="%~dp0rclone.exe"
 SET RCLONE_CONFIG_PATH="%~dp0rclone.conf"
+REM Paths
 SET RCLONE_REMOTE_PATH=pCloudEncrypted:
 SET RCLONE_LOCAL_DRIVE=X:
 REM Buffer size when copying files (default 16M)
@@ -15,6 +16,13 @@ REM IO idle timeout (default 5m0s)
 SET RCLONE_IO_IDLE_TIMEOUT=5s
 REM Connect timeout (default 1m0s)
 SET RCLONE_CONNECT_TIMEOUT=5s
+REM Number of low level retries to do (default 10)
+SET RCLONE_LOW_LEVEL_RETRIES_NUMBER=10
+REM Retry operations this many times if they fail (default 3)
+SET RCLONE_RETRIES_NUMBER=10
+REM Interval between retrying operations if they fail, e.g 500ms, 60s, 5m (0 to disable)
+SET RCLONE_RETRIES_SLEEP=0
+REM Additional rclone flags
 SET RCLONE_ADDITIONAL_FLAGS=--read-only
 
 REM Console height / width
@@ -44,7 +52,7 @@ SETLOCAL
 	ECHO.
 
 	REM Mount %RCLONE_REMOTE_PATH% on %RCLONE_LOCAL_DRIVE% drive letter
-	%RCLONE_EXE_PATH% mount %RCLONE_REMOTE_PATH% %RCLONE_LOCAL_DRIVE% --config=%RCLONE_CONFIG_PATH% %RCLONE_ADDITIONAL_FLAGS% --buffer-size %RCLONE_BUFFER_SIZE% --timeout %RCLONE_IO_IDLE_TIMEOUT% --contimeout %RCLONE_CONNECT_TIMEOUT%
+	%RCLONE_EXE_PATH% mount %RCLONE_REMOTE_PATH% %RCLONE_LOCAL_DRIVE% --config=%RCLONE_CONFIG_PATH% %RCLONE_ADDITIONAL_FLAGS% --buffer-size %RCLONE_BUFFER_SIZE% --timeout %RCLONE_IO_IDLE_TIMEOUT% --contimeout %RCLONE_CONNECT_TIMEOUT% --low-level-retries %RCLONE_LOW_LEVEL_RETRIES_NUMBER% --retries %RCLONE_RETRIES_NUMBER% --retries-sleep %RCLONE_RETRIES_SLEEP%
 ENDLOCAL
 REM Wait 10 seconds, then exit script
 TIMEOUT 10 | ECHO off
--- rclone-sync.bat
+++ rclone-sync.bat
@@ -12,12 +12,22 @@ REM Log level : DEBUG|INFO|NOTICE|ERROR (default NOTICE)
 SET RCLONE_LOG_LEVEL=INFO
 REM Read exclude patterns from file
 SET RCLONE_FILTER_FILE_PATH="%~dp0rclone-filters.txt"
+REM Number of checkers to run in parallel (default 8)
+SET RCLONE_CHECKERS_NUMBER=8
 REM Number of file transfers to run in parallel (default 4)
 SET RCLONE_FILE_TRANSFERS_NUMBER=4
+REM Number of low level retries to do (default 10)
+SET RCLONE_LOW_LEVEL_RETRIES_NUMBER=10
+REM Retry operations this many times if they fail (default 3)
+SET RCLONE_RETRIES_NUMBER=10
+REM Interval between retrying operations if they fail, e.g 500ms, 60s, 5m (0 to disable)
+SET RCLONE_RETRIES_SLEEP=5s
+REM Paths
 SET RCLONE_LOCAL_PATH=D:\
 SET RCLONE_REMOTE_PATH=pCloudEncrypted:
 REM Local directory names to sync, comma separated
 SET RCLONE_DIRECTORIES_TO_SYNC=Backups,Documents,Ebooks,Games,Movies,Music,Pictures,Softwares
+REM Additional rclone flags
 SET RCLONE_ADDITIONAL_FLAGS=--delete-excluded
 
 REM Console height / width
@@ -46,7 +56,7 @@ SETLOCAL
 	FOR %%A IN (%RCLONE_DIRECTORIES_TO_SYNC%) DO (
 		ECHO  - %RCLONE_LOCAL_PATH%%%A -^> %RCLONE_REMOTE_PATH%%%A
 		@ECHO %date%;%time%;%%A;start>> %RCLONE_LOG_FILE_PATH%
-		%RCLONE_EXE_PATH% sync "%RCLONE_LOCAL_PATH%%%A" "%RCLONE_REMOTE_PATH%%%A" --config=%RCLONE_CONFIG_PATH% --exclude-from=%RCLONE_FILTER_FILE_PATH% %RCLONE_ADDITIONAL_FLAGS% --log-file=%RCLONE_LOG_FILE_PATH% --log-level %RCLONE_LOG_LEVEL% --transfers=%RCLONE_FILE_TRANSFERS_NUMBER%
+		%RCLONE_EXE_PATH% sync "%RCLONE_LOCAL_PATH%%%A" "%RCLONE_REMOTE_PATH%%%A" --config=%RCLONE_CONFIG_PATH% --exclude-from=%RCLONE_FILTER_FILE_PATH% %RCLONE_ADDITIONAL_FLAGS% --log-file=%RCLONE_LOG_FILE_PATH% --log-level %RCLONE_LOG_LEVEL% --transfers=%RCLONE_FILE_TRANSFERS_NUMBER% --checkers %RCLONE_CHECKERS_NUMBER% --low-level-retries %RCLONE_LOW_LEVEL_RETRIES_NUMBER% --retries %RCLONE_RETRIES_NUMBER% --retries-sleep %RCLONE_RETRIES_SLEEP%
 		@ECHO %date%;%time%;%%A;end>> %RCLONE_LOG_FILE_PATH%
 	)
 ENDLOCAL
